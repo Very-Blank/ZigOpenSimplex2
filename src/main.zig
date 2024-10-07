d70ef9668d1bf67503c4cec9f974ee2b406194bc
blank <aapeli.saarelainen.76@gmail.com>
2024-10-07 18:52:24 +0300

    DONE! All noise functions for noise2, noise3 and noise4

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,2 @@
-const std = @import("std");
 const fast = @import("fast.zig");
-pub fn main() !void {
-    const noise = try fast.Noise4.init(340340, std.heap.page_allocator);
-    defer noise.deInit();
-    std.debug.print("{any}\n", .{noise.improveXYZW(50.0, 50.0, 100.0, 100.0)});
-}
+const smooth = @import("smooth.zig");
